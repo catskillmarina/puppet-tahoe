7c8236b935f57f5e1da8872e3fa8ab19d86e61d7
Marina Brown <catskillmarina@gmail.com>
2014-06-01 10:23:01 -0400

    Add Todo

--- manifests/ubuntu.pp
+++ manifests/ubuntu.pp
@@ -2,8 +2,9 @@ class tahoe::ubuntu inherits tahoe::base {
 
   case $::lsbdistcodename {
     maverick:  { $dist = 'maverick' }
-    lucid: { $dist = 'lucid' }
-    karmic: { $dist = 'karmic' }
+    lucid:     { $dist = 'lucid' }
+    karmic:    { $dist = 'karmic' }
+    precise:   { $dist = 'precise' }
     default: { fail "Unsupported distribution ${::lsbdistcodename}" }
   }
 
